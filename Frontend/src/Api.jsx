99741923e8b91d630bc0ca5510c7e47d871ed35c
vidhishree58 <vidhishree58@gmail.com>
2026-08-02 14:08:33 +0530

    Update production API URL

--- Frontend/src/Api.jsx
+++ Frontend/src/Api.jsx
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const API = axios.create({
-  baseURL: "http://localhost:5000/api",
+  baseURL: "https://bookverse-backend-uqft.onrender.com/api",
 });
 
 export default API;
